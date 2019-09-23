c68a914401fc81226ef464cce6ff13ed435c37ce
benjamin142857 <benjamin142857@outlook.com>
2019-09-23 10:54:12 +0800

    201909231054

---
+++ test/Test001.py
@@ -0,0 +1,10 @@
+"""
+    coding      : UTF-8
+    Environment : Python 3.6
+    Author      : Benjamin142857
+    Data        : 9/23/2019
+    Remark      : Test001
+"""
+
+if __name__ == '__main__':
+    pass
